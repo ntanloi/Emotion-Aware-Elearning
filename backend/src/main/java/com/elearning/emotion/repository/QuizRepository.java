3c9f6a988d9ca4d497a64a12d4dfc75cb130e2e0
Nguyen Tan Loi <nguyenloidt.052017@gmail.com>
2026-09-06 09:54:08 +0700

    delete Lesson Repository, Quizrepository, QuizResultRepository in backend repository

--- backend/src/main/java/com/elearning/emotion/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elearning.emotion.repository;
-
-import com.elearning.emotion.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, String> {
-}
--- backend/src/main/java/com/elearning/emotion/repository/QuizResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elearning.emotion.repository;
-
-import com.elearning.emotion.entity.QuizResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizResultRepository extends JpaRepository<QuizResult, String> {
-}
